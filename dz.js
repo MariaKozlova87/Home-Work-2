95521506dca10d0fe452f2db88264be1d577c480
MariaKozlova87 <64326665+MariaKozlova87@users.noreply.github.com>
2020-04-25 21:44:27 +0300

    123
    123

---
+++ dz.js
@@ -0,0 +1,31 @@
+
+// let arr = [
+//     obj.one = {
+//     fruits :'bananas',
+//     berries: 'cranberries',
+//     vegetables: 'broccoli',
+
+// },
+
+
+
+// obj.two = {
+//     fruits :'pears',
+//     berries: 'blueberries',
+//     vegetables: 'cucumber',
+    
+// },
+
+// obj.three = {
+//     fruits :'pineapples',
+//     berries: 'raspberries',
+//     vegetables: 'tomatoes',
+    
+// },
+// ];
+// console.log();
+
+document.getElementById('broccoli')
+.onclick = () => {
+    document.getElementById ('').hidden = true;
+};
